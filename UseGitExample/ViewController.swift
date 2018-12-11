ea23ebb096b0c746ce87dcfc59d1565075cc83db
Haden Olfers <Haden_Olfers@hadens-air>
2018-12-10 19:40:52 -0600

    Initial Commit

---
+++ UseGitExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UseGitExample
+//
+//  Created by Haden Olfers on 12/10/18.
+//  Copyright © 2018 Haden Olfers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
